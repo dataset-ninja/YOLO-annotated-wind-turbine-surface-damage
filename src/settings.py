8b2ee794a677ef607c2eabd6baeeda6f64963c5c
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-06-20 16:31:56 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -5,8 +5,8 @@ from dataset_tools.templates import AnnotationType, CVTask, Industry, License
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Wind Turbine 7"
+PROJECT_NAME_FULL: str = "YOLO Annotated Wind Turbine Surface Damage"
 
 ##################################
 # * After uploading to instance ##
